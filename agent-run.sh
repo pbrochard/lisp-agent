d0a2f037c4c22d582fa716a610c5d34f597b8cc9
Philippe Brochard <hocwp@free.fr>
2026-08-12 01:12:33 +0200

    disable rlwrap warnings

--- agent-run.sh
+++ agent-run.sh
@@ -2,4 +2,4 @@
 
 # Wrap SBCL in rlwrap for better command line editing and history support
 # https://gist.github.com/vindarel/2309154f4e751be389fa99239764c363
-rlwrap -r -i -b '()' sbcl --load load.lisp
+rlwrap -r -i -b '()' --no-warnings sbcl --load load.lisp
