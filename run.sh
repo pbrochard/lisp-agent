add20f328f6acdcca50960d1cc22b7bfb1e5541c
Philippe Brochard <hocwp@free.fr>
2026-08-18 09:25:34 +0200

    Allow to use git and mount the $HOME/src directory

--- run.sh
+++ run.sh
@@ -4,13 +4,11 @@ API_KEY_CLAUDE=$(gpg -d ~/.authinfo.gpg 2> /dev/null | awk '/api.anthropic.com/ 
 API_KEY_GEMINI=$(gpg -d ~/.authinfo.gpg 2> /dev/null | awk '/^machine generativelanguage.googleapis.com/ {print $6}')
 API_KEY_OPENROUTER=$(gpg -d ~/.authinfo.gpg 2> /dev/null | awk '/^machine openrouter.ai/ {print $6}')
 
-# -v /home/phil/src/adam:/agent/data/src/adam
-
-
 docker run -it --rm \
 	   -e API_KEY_OPENROUTER=$API_KEY_OPENROUTER \
 	   -e API_KEY_GEMINI=$API_KEY_GEMINI \
 	   -e API_KEY_CLAUDE=$API_KEY_CLAUDE \
 	   -v "$(pwd)/data:/agent/data" \
 	   -v "$(pwd)/skills:/agent/skills" \
+	   -v "$HOME/src:/agent/data/src/" \
 	   lisp-agent
