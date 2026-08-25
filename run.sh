a6f904f5fa309966c4715d5c6dc4a8924237659d
Philippe Brochard <hocwp@free.fr>
2026-08-25 10:06:12 +0200

    allow to use Ollama

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ API_KEY_GEMINI=$(gpg -d ~/.authinfo.gpg 2> /dev/null | awk '/^machine generative
 API_KEY_OPENROUTER=$(gpg -d ~/.authinfo.gpg 2> /dev/null | awk '/^machine openrouter.ai/ {print $6}')
 
 docker run -it --rm \
+	   --network=host \
 	   -e API_KEY_OPENROUTER=$API_KEY_OPENROUTER \
 	   -e API_KEY_GEMINI=$API_KEY_GEMINI \
 	   -e API_KEY_CLAUDE=$API_KEY_CLAUDE \
