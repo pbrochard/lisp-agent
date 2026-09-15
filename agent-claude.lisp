494b09b9e32179e477d69d08c4d2314ac8357da2
Philippe Brochard <hocwp@free.fr>
2026-09-15 10:44:32 +0200

    *: add grey color for model status

--- agent-claude.lisp
+++ agent-claude.lisp
@@ -116,7 +116,7 @@
                   (agent-loop
                    (append (recall)
                            (list (obj "role" "user" "content" prompt)))))))
-    (format t "~&______~&~%~a~%~a ~a~%" (final-text (car (last history))) SEP *model*)
+    (format t "~&______~&~%~a~%~a ~a~%" (final-text (car (last history))) SEP (grey *model*))
 	(set-status STATUS-OK)))
 
 (defun use ()
