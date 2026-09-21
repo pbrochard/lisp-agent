abafb7a6120fd743268fc708e685a1e38539b7ae
Philippe Brochard <hocwp@free.fr>
2026-09-21 16:39:55 +0200

    fat (agent-run): record the session

--- agent-run.sh
+++ agent-run.sh
@@ -13,5 +13,7 @@ echo "[AI]" > $STATUS_FILE
 
 # Wrap SBCL in rlwrap for better command line editing and history support
 # https://gist.github.com/vindarel/2309154f4e751be389fa99239764c363
-rlwrap -r -i -b '()' --no-warnings sbcl --load load.lisp --eval '(in-package :common)'
-
+# To filter out colors:
+#   `tail -F session-out.log | ansifilter`
+#   `ansifilter session-out.log > session-out-mono.log`
+script -q -f -c "rlwrap -r -i -b '()' --no-warnings sbcl --load load.lisp --eval '(in-package :common)'" ./data/session-out.log
